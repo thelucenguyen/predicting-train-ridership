8ac7bc1899f09ab062caee526229cf796d85f2b5
Lucille Eleanor "Ellie" Nguyen <88404349+thelucenguyen@users.noreply.github.com>
2025-09-14 13:20:05 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,3 @@
-import '../styles/globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <div className={inter.className}>
-      <Component {...pageProps} />
-    </div>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
