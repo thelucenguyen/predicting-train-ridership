2eb4479a710c7306adf88cb42114a3a6b222d0a4
Lucille Eleanor "Ellie" Nguyen <88404349+thelucenguyen@users.noreply.github.com>
2025-09-14 02:20:33 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,31 +1,4 @@
-const { copyFileSync, existsSync, mkdirSync } = require('fs');
-const { join } = require('path');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (!dev && !isServer) {
-      // Copy routes-manifest.json to the output directory during build
-      config.plugins.push({
-        apply: (compiler) => {
-          compiler.hooks.afterEmit.tap('CopyRoutesManifest', (compilation) => {
-            const srcPath = join(process.cwd(), 'routes-manifest.json');
-            const outDir = join(process.cwd(), '.next');
-            const destPath = join(outDir, 'routes-manifest.json');
-            
-            if (existsSync(srcPath)) {
-              if (!existsSync(outDir)) {
-                mkdirSync(outDir, { recursive: true });
-              }
-              copyFileSync(srcPath, destPath);
-              console.log('✓ Copied routes-manifest.json');
-            }
-          });
-        },
-      });
-    }
-    return config;
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
